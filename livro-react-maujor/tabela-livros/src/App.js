c11bdbb7c3f63294d0b9d4d6597a1db8e358dba2
Juliano <juliano.co.lopes@gmail.com>
2022-02-13 21:16:59 -0300

    projeto tabela livros

--- livro-react-maujor/tabela-livros/src/App.js
+++ livro-react-maujor/tabela-livros/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TabelaHead from "./components/TabelaHead";
+import TabelaBody from "./components/TabelaBody";
+import TabelaFoot from "./components/TabelaFoot"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    livros: []
+  };
+
+  componentDidMount() {
+    fetch("/api/livros.json")
+      .then(response => response.json())
+      .then(livros => this.setState({ livros }))
+      .catch(function(error) {
+        console.log("Erro na requisição");
+      })
+      .finally(function() {
+        console.log("Sempre retorna");
+      });
+  }
+
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaBody livros={ this.state.livros } />
+        <TabelaFoot qdeLivros = { this.state.livros.length } />
+      </table>
+    );
+  }
 }
 
 export default App;
